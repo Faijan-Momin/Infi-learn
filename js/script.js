4126e99b3c7358ce7ac582053727809c22b5ad43
Faijan-Momin <faijanmomin99@gmail.com>
2021-04-29 20:52:19 +0530

    first commit

--- js/script.js
+++ js/script.js
@@ -47,3 +47,79 @@ function showTechContent(nameofdiv){
         document.getElementById('teacher-sec-back-btn').style.display ="none";
     }
 }
+
+// Admin Panel Dashboard
+window.onload = function showAdminPanellis(){
+    var manage_user = document.getElementById('admin-panel-manage-user').style.display ="block";
+    var manage_video = document.getElementById('admin-panel-manage-video').style.display ="none";
+    var manage_file = document.getElementById('admin-panel-manage-file').style.display ="none";
+    var manage_req = document.getElementById('admin-panel-manage-request').style.display ="none";
+    var manage_analytics = document.getElementById('admin-panel-manage-analytics').style.display ="none";
+    var upload_vid = document.getElementById('admin-sub-upload-vid').style.display ="none";
+    var upload_file = document.getElementById('admin-sub-upload-file').style.display ="none";
+}
+function showAdminPanelli(panellink){
+    if (panellink == "manage-user") {
+        var manage_user = document.getElementById('admin-panel-manage-user').style.display ="block";
+        var manage_video = document.getElementById('admin-panel-manage-video').style.display ="none";
+        var manage_file = document.getElementById('admin-panel-manage-file').style.display ="none";
+        var manage_req = document.getElementById('admin-panel-manage-request').style.display ="none";
+        var manage_analytics = document.getElementById('admin-panel-manage-analytics').style.display ="none";
+        var upload_vid = document.getElementById('admin-sub-upload-vid').style.display ="none";
+        var upload_file = document.getElementById('admin-sub-upload-file').style.display ="none";
+    } 
+    if (panellink == "manage-video") {
+        var manage_user = document.getElementById('admin-panel-manage-user').style.display ="none";
+        var manage_video = document.getElementById('admin-panel-manage-video').style.display ="block";
+        var manage_file = document.getElementById('admin-panel-manage-file').style.display ="none";
+        var manage_req = document.getElementById('admin-panel-manage-request').style.display ="none";
+        var manage_analytics = document.getElementById('admin-panel-manage-analytics').style.display ="none";
+        var upload_vid = document.getElementById('admin-sub-upload-vid').style.display ="none";
+        var upload_file = document.getElementById('admin-sub-upload-file').style.display ="none";
+    } 
+    if (panellink == "manage-file") {
+        var manage_user = document.getElementById('admin-panel-manage-user').style.display ="none";
+        var manage_video = document.getElementById('admin-panel-manage-video').style.display ="none";
+        var manage_file = document.getElementById('admin-panel-manage-file').style.display ="block";
+        var manage_req = document.getElementById('admin-panel-manage-request').style.display ="none";
+        var manage_analytics = document.getElementById('admin-panel-manage-analytics').style.display ="none";
+        var upload_vid = document.getElementById('admin-sub-upload-vid').style.display ="none";
+        var upload_file = document.getElementById('admin-sub-upload-file').style.display ="none";
+    } 
+    if (panellink == "manage-req") {
+        var manage_user = document.getElementById('admin-panel-manage-user').style.display ="none";
+        var manage_video = document.getElementById('admin-panel-manage-video').style.display ="none";
+        var manage_file = document.getElementById('admin-panel-manage-file').style.display ="none";
+        var manage_req = document.getElementById('admin-panel-manage-request').style.display ="block";
+        var manage_analytics = document.getElementById('admin-panel-manage-analytics').style.display ="none";
+        var upload_vid = document.getElementById('admin-sub-upload-vid').style.display ="none";
+        var upload_file = document.getElementById('admin-sub-upload-file').style.display ="none";
+    } 
+    if (panellink == "manage-analytics") {
+        var manage_user = document.getElementById('admin-panel-manage-user').style.display ="none";
+        var manage_video = document.getElementById('admin-panel-manage-video').style.display ="none";
+        var manage_file = document.getElementById('admin-panel-manage-file').style.display ="none";
+        var manage_req = document.getElementById('admin-panel-manage-request').style.display ="none";
+        var manage_analytics = document.getElementById('admin-panel-manage-analytics').style.display ="block";
+        var upload_vid = document.getElementById('admin-sub-upload-vid').style.display ="none";
+        var upload_file = document.getElementById('admin-sub-upload-file').style.display ="none";
+    } 
+    if (panellink == "upload-vid") {
+        var manage_user = document.getElementById('admin-panel-manage-user').style.display ="none";
+        var manage_video = document.getElementById('admin-panel-manage-video').style.display ="none";
+        var manage_file = document.getElementById('admin-panel-manage-file').style.display ="none";
+        var manage_req = document.getElementById('admin-panel-manage-request').style.display ="none";
+        var manage_analytics = document.getElementById('admin-panel-manage-analytics').style.display ="none";
+        var upload_vid = document.getElementById('admin-sub-upload-vid').style.display ="block";
+        var upload_file = document.getElementById('admin-sub-upload-file').style.display ="none";
+    } 
+    if (panellink == "upload-file") {
+        var manage_user = document.getElementById('admin-panel-manage-user').style.display ="none";
+        var manage_video = document.getElementById('admin-panel-manage-video').style.display ="none";
+        var manage_file = document.getElementById('admin-panel-manage-file').style.display ="none";
+        var manage_req = document.getElementById('admin-panel-manage-request').style.display ="none";
+        var manage_analytics = document.getElementById('admin-panel-manage-analytics').style.display ="none";
+        var upload_vid = document.getElementById('admin-sub-upload-vid').style.display ="none";
+        var upload_file = document.getElementById('admin-sub-upload-file').style.display ="block";
+    } 
+}
